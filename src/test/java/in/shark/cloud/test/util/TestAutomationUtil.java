2b4e54cee0f041e47c7be360e1941c3782eae363
Surabhi <surabhi@chaipoint.com>
2019-11-25 18:32:46 +0530

    initial commit

---
+++ src/test/java/in/shark/cloud/test/util/TestAutomationUtil.java
@@ -0,0 +1,22 @@
+package in.shark.cloud.test.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class TestAutomationUtil {
+	private static Logger logger = LoggerFactory.getLogger(TestAutomationUtil.class);
+	
+	public static String getContent(String fileName) {
+		String content = null;
+		try {
+			content = new String(Files.readAllBytes(Paths.get(fileName)));
+		} catch (IOException e) {
+			logger.error("Error printing from file: " + fileName, e);
+		}
+		return content;
+	}
+}
---
+++ src/test/java/in/shark/online/web/test/catalogue/master/TestGetProductCatalogueApiTestNg.java
@@ -0,0 +1,88 @@
+package in.shark.online.web.test.catalogue.master;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.simple.parser.ParseException;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.BodyContentSpec;
+import org.springframework.test.web.reactive.server.WebTestClient.RequestBodyUriSpec;
+import org.springframework.test.web.reactive.server.WebTestClient.RequestHeadersUriSpec;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import in.shark.online.web.test.TestApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestApplication.class)
+@ActiveProfiles("qa")
+@AutoConfigureWebTestClient
+public class TestGetProductCatalogueApiTestNg extends AbstractTestNGSpringContextTests{
+	
+	@Value("${server.url}")
+	private String serverUrl;
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	private void addHeaders(RequestHeadersUriSpec<?> requestBody, String apiKey) {
+		requestBody.header("content-type", "application/json;charset=UTF-8");
+		requestBody.header("service.auth.key", apiKey);
+	}
+
+	private String getUrl(String apiUri) {
+		return serverUrl + apiUri;
+	}
+	@Test
+	@Parameters({"apiKey", "apiUri","moduleId"})
+	public void testGetProductCatalogue(String apiKey,String apiUri, String moduleId ) {
+		
+		logger.info("Running GetProductCatalogue");
+
+		String url = getUrl(apiUri+"?moduleId="+moduleId);
+		WebTestClient testClient = WebTestClient.bindToServer().baseUrl(url).build();
+		RequestBodyUriSpec requestBody = testClient.post();
+		addHeaders(requestBody, apiKey);
+		BodyContentSpec body = requestBody.contentType(MediaType.APPLICATION_JSON).syncBody(moduleId).exchange()
+				.expectStatus().isOk().expectBody();
+
+		logger.info("Completed GetProductCatalogue");
+	}
+	@Test(invocationCount = 100, threadPoolSize = 10)
+	@Parameters({ "apiKey", "apiUri", "moduleId", "maxResponseTimeThreshold" })
+	public void testGetProductCatalogueLoad(String apiKey, String apiUri, String moduleId, Long maxResponseTimeThreshold)
+			throws JsonParseException, JsonMappingException, IOException, JSONException, ParseException {
+		logger.info("Running testGetProductCatalogueLoad");
+		logger.info("responseTimeThreshold: " + maxResponseTimeThreshold);
+		Long startTime = System.currentTimeMillis();
+		getProductCatalogue(apiKey, apiUri, moduleId);
+		Long duration = System.currentTimeMillis() - startTime;
+		assertTrue(duration <= maxResponseTimeThreshold,
+				"Response time of GetProductCatalogue call more than: " + maxResponseTimeThreshold);
+		logger.info("Completed testGetProductCatalogueLoad");
+	}
+	
+	public void getProductCatalogue(String apiKey,String apiUri, String moduleId ) {
+		String url = getUrl(apiUri+"?moduleId="+moduleId);
+		WebTestClient testClient = WebTestClient.bindToServer().baseUrl(url).build();
+		RequestBodyUriSpec requestBody = testClient.post();
+		addHeaders(requestBody, apiKey);
+		BodyContentSpec body = requestBody.contentType(MediaType.APPLICATION_JSON).syncBody(moduleId).exchange()
+				.expectStatus().isOk().expectBody();
+	}
+
+}
